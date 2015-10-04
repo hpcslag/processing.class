4beae96924178a5d22758659624841ec0956ba81
MacTaylor <cslag@hotmail.com.tw>
2015-10-04 14:54:09 +0800

    Create Rectangle.c

---
+++ 3.Shape/Rectangle.c
@@ -0,0 +1,18 @@
+//french flag
+void setup(){
+  size(1280, 720);
+  noStroke();
+}
+
+void draw(){
+  background(255);
+  
+  
+  //left rectangle
+  fill(0, 30, 150);
+  rect(0, 0, 160, 320);
+  
+  //right rectangle
+  fill(210, 30, 30);
+  rect(320, 0, 160, 320);
+}
